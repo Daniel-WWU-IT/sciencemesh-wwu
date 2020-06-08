5aa6aefc1de0b867a8c6bfb92a59171d1c31e308
Daniel Müller <daniel.mueller@uni-muenster.de>
2020-06-08 16:45:54 +0200

    Added PVC to Prometheus

--- iop/prom_install.sh
+++ iop/prom_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+kubectl apply -f k8s/prom-shared-data-pvc.yaml
 helm install prometheus stable/prometheus -f charts/prometheus.yaml
--- iop/prom_remove.sh
+++ iop/prom_remove.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 helm delete prometheus
+kubectl delete -f k8s/prom-shared-data-pvc.yaml
