888d4167d14edfb2ef94aab99c6a3a337eac8096
Daniel Mueller <daniel.mueller@uni-muenster.de>
2020-10-27 15:44:45 +0100

    Rename config/iop to config/reva

--- iop/iop_install.sh
+++ iop/iop_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 kubectl apply -f k8s/iop-data-pvc.yaml
 helm install iop sciencemesh/iop \
-    --set-file gateway.configFiles.revad\\.toml=config/iop/gateway.toml \
-    --set-file gateway.configFiles.users\\.json=config/iop/users.json \
-    --set-file storageprovider-home.configFiles.revad\\.toml=config/iop/storage-home.toml \
-    --set-file storageprovider-reva.configFiles.revad\\.toml=config/iop/storage-reva.toml \
+    --set-file gateway.configFiles.revad\\.toml=config/reva/gateway.toml \
+    --set-file gateway.configFiles.users\\.json=config/reva/users.json \
+    --set-file storageprovider-home.configFiles.revad\\.toml=config/reva/storage-home.toml \
+    --set-file storageprovider-reva.configFiles.revad\\.toml=config/reva/storage-reva.toml \
     -f charts/iop.yaml
