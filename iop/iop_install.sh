309e66f8ac08159dcbb7c2842a43326e5e5e1f55
Daniel Müller <daniel.mueller@uni-muenster.de>
2020-11-30 13:35:17 +0100

    Make Mentix localfile connector persistent

--- iop/iop_install.sh
+++ iop/iop_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kubectl apply -f k8s/iop-data-pvc.yaml
+kubectl apply -f k8s/mentix-data-pvc.yaml
 helm install iop sciencemesh/iop \
     --set-file gateway.configFiles.revad\\.toml=config/reva/gateway.toml \
     --set-file gateway.configFiles.users\\.json=config/reva/users.json \
--- iop/iop_remove.sh
+++ iop/iop_remove.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 helm delete iop
-# Uncomment to delete the PVC
+# Uncomment to delete the PVCs
 # kubectl delete -f k8s/iop-data-pvc.yaml
+# kubectl delete -f k8s/mentix-data-pvc.yaml
