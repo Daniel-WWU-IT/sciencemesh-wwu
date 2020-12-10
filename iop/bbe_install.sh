4470829c7ffad531a88222241edc05e71b10c883
Daniel Müller <daniel.mueller@uni-muenster.de>
2020-12-10 14:56:49 +0100

    Add Prometheus recording rules

--- iop/bbe_install.sh
+++ iop/bbe_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Merge the chart's values file and the BBE configuration into one; this allows the BBE configuration to be kept in a separate file
-cat charts/blackbox-exporter.yaml > charts/blackbox-exporter.merged.yaml
-cat config/blackbox-exporter.yaml | sed -e 's/^/  /' >> charts/blackbox-exporter.merged.yaml
+cp charts/blackbox-exporter.yaml charts/blackbox-exporter.merged.yaml
+cp config/blackbox-exporter.yaml config/blackbox-exporter.merged.yaml
 
-helm install blackbox-exporter prometheus-community/prometheus-blackbox-exporter -f charts/blackbox-exporter.merged.yaml
+sed -i -e 's/^/  /' config/blackbox-exporter.merged.yaml
+sed -i -e '/MODULES/r config/blackbox-exporter.merged.yaml' charts/blackbox-exporter.merged.yaml
+
+helm install blackbox-exporter prometheus-community/prometheus-blackbox-exporter \
+    -f charts/blackbox-exporter.merged.yaml
 
 rm -f charts/blackbox-exporter.merged.yaml
+rm -f config/blackbox-exporter.merged.yaml
