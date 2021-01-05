5966154d8c40d163495f8dee7959da061a4bdd5d
Daniel Müller <daniel.mueller@uni-muenster.de>
2021-01-05 15:51:51 +0100

    Add check history dashboard

--- iop/grafana_install.sh
+++ iop/grafana_install.sh
@@ -2,4 +2,5 @@
 helm install grafana grafana/grafana \
     --set-file dashboards.sciencemesh.general-statistics.json=config/grafana/general-statistics.json \
     --set-file dashboards.sciencemesh.health-monitoring.json=config/grafana/health-monitoring.json \
+    --set-file dashboards.sciencemesh.check-history.json=config/grafana/check-history.json \
     -f charts/grafana.yaml
