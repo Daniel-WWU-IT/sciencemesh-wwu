7f368d9ad6ecd226aad0706c4400c45e01c0c448
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-04-17 11:22:14 +0200

    Update TLS

--- iop/create_tls_secret.sh
+++ iop/create_tls_secret.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl delete secret sciencemesh-tls
 # Chain consists of Server cert + Chain + Root
-kubectl create secret tls sciencemesh-tls --cert=sciencemesh-chain.cert --key=sciencemesh.key
-
+# PEM file: First part (up to ~L53) = key; Remaining stuff = cert
+kubectl create secret tls sciencemesh-tls --cert=sciencemesh.cert --key=sciencemesh.key
